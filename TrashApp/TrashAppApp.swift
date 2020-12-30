c986b8d3be87cbdc6b3dbbff595007ceeabddc5f
Brad Gessler <bradgessler@gmail.com>
2020-12-30 10:14:38 -0800

    Initial Commit

---
+++ TrashApp/TrashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrashAppApp.swift
+//  TrashApp
+//
+//  Created by Brad Gessler on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct TrashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
