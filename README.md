87586b39e34de7ce26d37f53fb763c70de9a5c17
Brad Gessler <bradgessler@gmail.com>
2020-12-31 14:28:45 -0800

    SwankWebView, oh my!

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # TrashApp
 
+This repo is a journal of me learning how SwiftUI, Swift, and WKWebKit works together. My goal is to build out a very flexible MIT licenses SwiftUI `WebView` component that makes building hybrid web/native apps a really easy thing to do with SwiftUI and minimal coding. The format of this particular repo is a journal.
+
+## December 31, 2020
+
+https://developer.apple.com/forums/thread/126986 is headed down the right path of what I'm looking for in terms of not allowing WKWebView to manage the history and navigation of links. This will make it possible to put a `WebView` in a `NavigationView` to handle history, etc. What I need to figure out is how to intercept the URL, HTTP response object, etc. in the callback so I can do certain actions like load PDFs into a PDF viewer instead of the WKWebKit view. For now, I'm going to call this the `SwankyWebView`.
+
+## December 30, 2020
+
 Learning SwiftUI by trying to build out a `WebView("https://www.google.com/")` view component that I can plugin into the built-in SwiftUI `NavigationView`. The closest component I could find that does this is https://github.com/kylehickinson/SwiftUI-WebView, but it navigates within WKWebView. I want a component with less state then that; specifically I want it such that when a link is clicked on my `WebView` component, it doesn't actually navigate to the new URL. Instead the state of the `WebView` component should trigger a URL change that can be picked up by SwiftUI and acted upon. For this particular project, when a user clicks on a link, I want to push a new `WebView` component into the `NavigationView` stack.
 
 This repo doesn't quite do that yet. If you watch [this movie](https://s3.amazonaws.com/bradgessler/JXaUDf9aM9Kz5UqVwjMKkSgE4fdhIUWwkw781p1Oe4Vhio4W3gDKpdiiB9j2xZLjAFGrXvckwMY0ji82QKSmXoG0PtZ1BftrnOX1.mov) you'll see when I click on the "Load next page" button, the `NavigationView` controller is used. When I click on a link, its not used. I'd like to intercept these clicks to push the new view onto the `NavigationView` stack.
@@ -39,5 +47,5 @@ struct ContentView: View {
         }
     }
 }
-
 ```
+
---
+++ TrashApp/SwankyWebView.swift
@@ -0,0 +1,89 @@
+// From https://developer.apple.com/forums/thread/126986
+
+import SwiftUI
+import WebKit
+
+struct SwankyWebView: UIViewRepresentable {
+    @Binding var title: String
+    var url: URL
+    var loadStatusChanged: ((Bool, Error?) -> Void)? = nil
+
+    func makeCoordinator() -> SwankyWebView.Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let view = WKWebView()
+        view.navigationDelegate = context.coordinator
+        view.load(URLRequest(url: url))
+        return view
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // you can access environment via context.environment here
+        // Note that this method will be called A LOT
+    }
+
+    func onLoadStatusChanged(perform: ((Bool, Error?) -> Void)?) -> some View {
+        var copy = self
+        copy.loadStatusChanged = perform
+        return copy
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        let parent: SwankyWebView
+
+        init(_ parent: SwankyWebView) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+            parent.loadStatusChanged?(true, nil)
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.title = webView.title ?? ""
+            parent.loadStatusChanged?(false, nil)
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            parent.loadStatusChanged?(false, error)
+        }
+    }
+}
+
+struct Display: View {
+    @State var title: String = ""
+    @State var error: Error? = nil
+
+    var body: some View {
+        NavigationView {
+            SwankyWebView(title: $title, url: URL(string: "https://www.apple.com/")!)
+                .onLoadStatusChanged { loading, error in
+                    if loading {
+                        print("Loading started")
+                        self.title = "Loading…"
+                    }
+                    else {
+                        print("Done loading.")
+                        if let error = error {
+                            self.error = error
+                            if self.title.isEmpty {
+                                self.title = "Error"
+                            }
+                        }
+                        else if self.title.isEmpty {
+                            self.title = "Some Place"
+                        }
+                    }
+            }
+            .navigationBarTitle(title)
+        }
+    }
+}
+
+struct SwankyWebView_Previews: PreviewProvider {
+    static var previews: some View {
+        Display()
+    }
+}
